c62721010be6e71b1cf4182f7c8a2810075f2404
Will <wor101@gmail.com>
2020-10-09 20:00:13 +0000

    Final rock_paper_scissors_spock_lizzard.rb

--- lesson_2/rock_paper_scissors_spock_lizzard.rb
+++ lesson_2/rock_paper_scissors_spock_lizzard.rb
@@ -1,13 +1,23 @@
-VALID_CHOICES = ['rock', 'paper', 'scissors']
+VALID_CHOICES = ['rock', 'paper', 'scissors', 'lizard', 'spock']
+
+VALID_SHORTHAND_CHOICES = { 'r' => 'rock',
+                            'p' => 'paper',
+                            'sc' => 'scissors',
+                            'l' => 'lizard',
+                            'sp' => 'spock' }
+
+OUTCOMES = {  'rock' => ['lizard', 'scissors'],
+              'paper' => ['rock', 'spock'],
+              'scissors' => ['paper', 'lizard'],
+              'lizard' => ['spock', 'paper'],
+              'spock' => ['scissors', 'rock'] }
 
 def prompt(message)
   Kernel.puts("=> #{message}")
 end
 
 def win?(first, second)
-  (first == 'rock' && second == 'scissors') ||
-    (first == 'paper' && second == 'rock') ||
-    (first == 'scissors' && second == 'paper')
+  OUTCOMES[first].include?(second)
 end
 
 def display_results(player, computer)
@@ -22,25 +32,58 @@ def display_results(player, computer)
   result
 end
 
-loop do
-  choice = ''
-  loop do
-    prompt("Choose one: #{VALID_CHOICES.join(', ')}")
-    choice = Kernel.gets().chomp()
+def update_score(player, computer, array)
+  if win?(player, computer)
+    array[0] += 1
+  elsif win?(computer, player)
+    array[1] += 1
+  end
+  array
+end
 
-    if VALID_CHOICES.include?(choice)
+def validate_choice(string)
+  if VALID_CHOICES.include?(string)
+    string
+  elsif VALID_SHORTHAND_CHOICES.key?(string)
+    VALID_SHORTHAND_CHOICES[string]
+  else
+    puts "Invalid choice."
+    puts "If using shorthand: #{VALID_SHORTHAND_CHOICES.keys.join(', ')}"
+  end
+end
+
+prompt("Welcome to Rock, Paper, Scissors, Lizard, Spock!")
+puts ""
+
+loop do # main loop
+  prompt("The first player to 5 is the Grand Winner!")
+
+  score = [0, 0]
+  loop do # match loop, first to five is the winner
+    choice = ''
+
+    loop do # validate user input loop
+      prompt("Choose one: #{VALID_CHOICES.join(', ')}")
+      choice = Kernel.gets().chomp()
+      choice = validate_choice(choice)
+      break if choice
+    end
+
+    computer_choice = VALID_CHOICES.sample
+    Kernel.puts("You chose: #{choice}; Computer chose: #{computer_choice}")
+    puts display_results(choice, computer_choice)
+
+    score = update_score(choice, computer_choice, score) # update score
+    puts "Current Score: You #{score[0]} vs. Computer #{score[1]}"
+
+    if score[0] == 5
+      prompt("You are the Grand Winner!")
       break
-    else
-      prompt("That's not a valid choice.")
+    elsif score[1] == 5
+      prompt("The computer is the Grand Winner!")
+      break
     end
   end
-
-  computer_choice = VALID_CHOICES.sample
-
-  Kernel.puts("You chose: #{choice}; Computer chose: #{computer_choice}")
-
-  puts display_results(choice, computer_choice)
-
   prompt("Do you want to play again?")
   answer = Kernel.gets().chomp()
   break unless answer.downcase().start_with?('y')
