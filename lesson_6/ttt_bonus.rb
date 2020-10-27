02c3e2f703980dd3ba7609a60a9235f23812a941
Will <wor101@gmail.com>
2020-10-27 23:51:43 +0000

    more ttt_bonus features

--- lesson_6/ttt_bonus.rb
+++ lesson_6/ttt_bonus.rb
@@ -1,37 +1,60 @@
 require 'pry'
 
+FIRST_MOVE = 'choose'
 INITIAL_MARKER = ' '
 PLAYER_MARKER = 'X'
 COMPUTER_MARKER = 'O'
 WINNING_LINES = [[1, 2, 3], [4, 5, 6], [7, 8, 9]] + # rows
                 [[1, 4, 7], [2, 5, 8], [3, 6, 9]] + # columns
                 [[1, 5, 9], [3, 5, 7]] # diagnols
-score = {'Player' => 0, 'Computer' => 0}
+
+score = { 'Player' => 0, 'Computer' => 0 }
+current_match = 0
 
 def prompt(msg)
   puts "=> #{msg}"
 end
 
+def greeting
+  first_player = FIRST_MOVE
+  system 'clear'
+  puts "Welcome to Tic Tac Toe!"
+  puts ""
+
+  if FIRST_MOVE == "choose"
+    loop do
+      prompt "Please choose who goes first (player or computer): "
+      answer = gets.chomp.downcase
+      if  answer == 'player' || answer == 'computer'
+        first_player = answer
+        system 'clear'
+        break
+      end
+    end
+  end
+
+  first_player
+end
+
 def joinor(input_array, delimeter = ', ', final_delimeter = 'or')
   joined_string = ''
-  
-  if input_array.length <= 2 
+
+  if input_array.length <= 2
     return joined_string = input_array.join(' ' + final_delimeter + ' ')
   end
-  
+
   input_array.each_with_index do |int, index|
-    if input_array[index] != input_array.last 
+    if input_array[index] != input_array.last
       joined_string << int.to_s + delimeter
     else
       joined_string << final_delimeter + ' ' + int.to_s
     end
   end
-  joined_string 
+  joined_string
 end
 
-
 # rubocop:disable Metrics/AbcSize
-def display_board(brd, current_score = {'Player' => 0, 'Computer' => 0})
+def display_board(brd, current_score = { 'Player' => 0, 'Computer' => 0 })
   system 'clear'
   puts display_score(current_score)
   puts "You're a #{PLAYER_MARKER}. Computer is a #{COMPUTER_MARKER}"
@@ -61,6 +84,18 @@ def empty_squares(brd)
   brd.keys.select { |num| brd[num] == INITIAL_MARKER }
 end
 
+def alternate_player(current_player)
+  current_player == 'player' ? 'computer' : 'player'
+end
+
+def place_piece!(brd, current_player, match_number)
+  if current_player == 'player'
+    player_places_piece!(brd)
+  else
+    computer_places_piece!(brd, match_number)
+  end
+end
+
 def player_places_piece!(brd)
   square = ''
   loop do
@@ -72,12 +107,12 @@ def player_places_piece!(brd)
   brd[square] = PLAYER_MARKER
 end
 
-def computer_places_piece!(brd)
+def computer_places_piece!(brd, match_number)
   if immediate_offense_defense(brd, COMPUTER_MARKER)
     square = immediate_offense_defense(brd, COMPUTER_MARKER)
   elsif immediate_offense_defense(brd, PLAYER_MARKER)
     square = immediate_offense_defense(brd, PLAYER_MARKER)
-  elsif empty_squares(brd).include?(5)
+  elsif empty_squares(brd).include?(5) && match_number > 1
     square = 5
   else
     square = empty_squares(brd).sample
@@ -85,43 +120,22 @@ def computer_places_piece!(brd)
   brd[square] = COMPUTER_MARKER
 end
 
+# need to simplify method to and probably break out into additional methods
 def immediate_offense_defense(brd, marker)
   next_move = nil
   WINNING_LINES.each do |winning_line|
-    if (brd[winning_line[0]] == marker) && (brd[winning_line[1]] == marker) && (brd[winning_line[2]] == INITIAL_MARKER)
+    if (brd[winning_line[0]] == marker) &&
+       (brd[winning_line[1]] == marker) &&
+       (brd[winning_line[2]] == INITIAL_MARKER)
       next_move = winning_line[2]
-    elsif (brd[winning_line[1]] == marker) && (brd[winning_line[2]] == marker) && (brd[winning_line[0]] == INITIAL_MARKER)
+    elsif (brd[winning_line[1]] == marker) &&
+          (brd[winning_line[2]] == marker) &&
+          (brd[winning_line[0]] == INITIAL_MARKER)
       next_move = winning_line[0]
     end
   end
   next_move
 end
-
-# def immediate_defense(brd)
-#   next_move = nil
-#   WINNING_LINES.each do |winning_line|
-#     if (brd[winning_line[0]] == 'X') && (brd[winning_line[1]] == 'X') && (brd[winning_line[2]] == ' ')
-#       next_move = winning_line[2]
-#     elsif (brd[winning_line[1]] == 'X') && (brd[winning_line[2]] == 'X') && (brd[winning_line[0]] == ' ')
-#       next_move = winning_line[0]
-#     end
-#   end
-#   next_move
-# end
-
-# def immediate_offense(brd)
-#   next_move = nil
-#   WINNING_LINES.each do |winning_line|
-#     if (brd[winning_line[0]] == 'O') && (brd[winning_line[1]] == 'O') && (brd[winning_line[2]] == ' ')
-#       next_move = winning_line[2]
-#     elsif (brd[winning_line[1]] == 'O') && (brd[winning_line[2]] == 'O') && (brd[winning_line[0]] == ' ')
-#       next_move = winning_line[0]
-#     end
-#   end
-#   binding.pry
-#   next_move
-# end
-
 
 def board_full?(brd)
   empty_squares(brd).empty?
@@ -161,35 +175,19 @@ end
 
 def final_winner(current_score)
   winner = nil
-  current_score.each_pair { | player, score| winner = player if score == 5 }
+  current_score.each_pair { |player, score| winner = player if score == 5 }
   winner
 end
 
-# **Algorithm**
-# - create method called update_score()
-#   - update_score will update the score array by mutating the array
-#   - it will be passed the return value of detect_winner and the score hash
-#   - based on input it will update either the value of player or computer
-# - create a method called check for final_winner
-#   - it will take the score hash as an argument
-#   - if either the value of player or computer is 5 then it will return the key otherwise it will return nil
-# - if final_winner return is true then declare grand winner
-# - else repeate game loop
-
-
-
 loop do
   board = initialize_board
-  
-  
-  
+  current_match = 0
+  current_player = greeting
+
   loop do
-    display_board(board, score)
-
-    player_places_piece!(board)
-    break if someone_won?(board) || board_full?(board)
-
-    computer_places_piece!(board)
+    display_board(board)
+    place_piece!(board, current_player, current_match)
+    current_player = alternate_player(current_player)
     break if someone_won?(board) || board_full?(board)
   end
 
@@ -202,21 +200,15 @@ loop do
     prompt "It's a tie!"
   end
 
-
-  
+  prompt display_score(score)
   if final_winner(score)
-    prompt display_score(score)
     prompt "#{final_winner(score)} is the Grand Winner!"
     prompt "Play again? (y or n)"
-    answer = gets.chomp
-    break unless answer.downcase.start_with?('y')
   else
-    prompt display_score(score)
     prompt "Ready for the next match? (y or n)"
-    answer = gets.chomp
-    break unless answer.downcase.start_with?('y')
   end
-
+  answer = gets.chomp
+  break unless answer.downcase.start_with?('y')
 end
 
 prompt "Thanks for playing Tic Tac Toe! Good bye!"
