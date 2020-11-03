8f46be5415adcc2cf532d537a4f34cf3de716baa
Will <wor101@gmail.com>
2020-11-03 21:57:45 +0000

    adding diamonds.rb

--- small_problems/medium_1/1000_lights.rb
+++ small_problems/medium_1/1000_lights.rb
@@ -1,26 +1,23 @@
-def toggle(n)
-  switches = Array.new(n, 'off')
+def on_and_off(array, n)
+  switches = array
   round = 1
-  
   until round > n
     switches.each_with_index do |switch, index|
-      if (index + 1) % round == 0 
-        if switch == 'off'
-          switches[index] = 'on'
-        else
-          switches[index]  = 'off'
-        end
-      end
-      puts switch + " at index# #{index}"
-      
+      next if (index + 1) % round != 0 
+      switch == 'off' ? switches[index] = 'on' : switches[index]  = 'off'
     end
-    
-    puts "End of round #{round}"
-    puts "*********************"
-    puts ""
     round += 1
   end
-  p switches
+  switches
+end
+
+def toggle(n)
+  switches = Array.new(n, 'off')
+  switches = on_and_off(switches, n)
+  
+  on = []
+  switches.each_with_index { |switch, index| on << (index + 1) if switch == 'on' }
+  p on
 end
 
 
@@ -38,6 +35,8 @@ end
 
 
 
-p toggle(5) == ['on', 'off', 'off', 'on', 'off']
+p toggle(5) == [1, 4]
 
-p toggle(10) 
+p toggle(10) == [1, 4, 9]
+
+p toggle(1000) == [1, 4, 9, 16, 25, 36, 49, 64, 81, 100, 121, 144, 169, 196, 225, 256, 289, 324, 361, 400, 441, 484, 529, 576, 625, 676, 729, 784, 841, 900, 961]
