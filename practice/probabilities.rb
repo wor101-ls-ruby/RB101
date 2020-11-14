002439b8f9ff7c1ec956522fc532c8bece1f3161
Will <wor101@gmail.com>
2020-11-14 13:26:19 -0500

    aa

--- practice/probabilities.rb
+++ practice/probabilities.rb
@@ -1,20 +1,24 @@
 def die_rolls(target, dice, previous = [])
-  valid_rolls = previous
+  valid_rolls = []
 
   (1..6).each do |current_roll|
+    test_rolls = []
+    test_rolls << current_roll
     if current_roll == target && dice == 1
-      valid_rolls << current_roll
-    elsif (target - current_roll) != 0 && dice !=1
-      
-      valid_rolls << die_rolls((target - 1),(dice - 1), previous).flatten
-
+      valid_rolls = valid_rolls + test_rolls
+    elsif (target - current_roll) != 0 && dice !=1      
+      test_rolls << die_rolls((target - current_roll),(dice - 1)).flatten
     end
+    if test_rolls.flatten.sum == target
+      valid_rolls = valid_rolls + test_rolls
+    end
+
 
   end
 
   valid_rolls
 end
 
-p die_rolls(9, 2)
+p die_rolls(6, 2)
 # p die_rolls(6, 2)
 # p die_rolls(6, 3)
