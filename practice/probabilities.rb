d3a3d5a2f3586ec8d8c1d0a5eb8dbd7389ec3db6
Will <wor101@gmail.com>
2020-11-15 08:23:58 -0500

    working probabilities.rb

--- practice/probabilities.rb
+++ practice/probabilities.rb
@@ -2,17 +2,15 @@ def die_rolls(target, dice, previous = [])
   valid_rolls = []
 
   (1..6).each do |current_roll|
-    test_rolls = []
-    test_rolls << current_roll
-    if current_roll == target && dice == 1
-      valid_rolls = valid_rolls + test_rolls
-    elsif (target - current_roll) != 0 && dice !=1      
-      test_rolls << die_rolls((target - current_roll),(dice - 1)).flatten
-    end
-    if test_rolls.flatten.sum == target
-      valid_rolls = valid_rolls + test_rolls
-    end
 
+    if current_roll == target && dice == 1
+      valid_rolls = [[current_roll]]
+    elsif (target - current_roll) != 0 && dice !=1      
+      remaining_dice = die_rolls((target - current_roll),(dice - 1))
+      remaining_dice.each do |remaining_rolls|
+        valid_rolls << remaining_rolls.unshift(current_roll)
+      end
+    end
 
   end
 
@@ -21,4 +19,5 @@ end
 
 p die_rolls(6, 2)
 # p die_rolls(6, 2)
-# p die_rolls(6, 3)
+p die_rolls(6, 3)
+p die_rolls(9, 4)
