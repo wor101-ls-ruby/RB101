8a2105cf8c53da789b61e89b3da590fc262ee60a
Will <wor101@gmail.com>
2020-11-19 20:46:07 +0000

    merge_sort with lesson solution added

--- small_problems/advanced_1/merge_sort.rb
+++ small_problems/advanced_1/merge_sort.rb
@@ -123,6 +123,17 @@ def merge_sort(array)
 end
 
 
+def merge_sort_lesson(array)
+  return array if array.size == 1
+
+  sub_array_1 = array[0...array.size / 2]
+  sub_array_2 = array[array.size / 2...array.size]
+
+  sub_array_1 = merge_sort(sub_array_1)
+  sub_array_2 = merge_sort(sub_array_2)
+
+  merge(sub_array_1, sub_array_2)
+end
 
 p merge_sort([1])
 p merge_sort([3,2,1])
